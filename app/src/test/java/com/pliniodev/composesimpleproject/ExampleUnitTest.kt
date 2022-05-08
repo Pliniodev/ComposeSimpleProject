09e1faf3e8cd8a29ee9bbbda8e976a693df30494
Plinio Alencar <plinioprojetos@hotmail.com>
2022-05-08 20:51:57 -0300

    updating tests

--- app/src/test/java/com/pliniodev/composesimpleproject/ExampleUnitTest.kt
+++ app/src/test/java/com/pliniodev/composesimpleproject/ExampleUnitTest.kt
@@ -10,6 +10,32 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    //        val breed = BreedPresentation(
+//            altNames = "",
+//            id = 1,
+//            lifeSpan = "",
+//            referenceImageId = "",
+//            shortLegs = 1,
+//            suppressedTail = 1,
+//            wikipediaUrl = "",
+//            experimental = 1,
+//            hairless = 1,
+//            hypoallergenic = 1,
+//            name = "doguinho",
+//            natural = 1,
+//            origin = "",
+//            rare = 1,
+//            rex = 1,
+//            temperament = "",
+//            weight = WeightPresentation(imperial = "1", metric = "1"),
+//            height = HeightPresentation(imperial = "1", metric = "1"),
+//            image = ImagePresentation(
+//                height = 1199,
+//                id = "1",
+//                url = "https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg",
+//                width = "1600"
+//            ),
+//        )
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
