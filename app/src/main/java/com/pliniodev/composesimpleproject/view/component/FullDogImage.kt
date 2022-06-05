28a9b9325ae53a156a66177a34730b4588c8181f
Plinio Alencar <plinioprojetos@hotmail.com>
2022-06-05 11:12:39 -0300

    fixing progress indicator

--- app/src/main/java/com/pliniodev/composesimpleproject/view/component/FullDogImage.kt
+++ app/src/main/java/com/pliniodev/composesimpleproject/view/component/FullDogImage.kt
@@ -1,8 +1,11 @@
 package com.pliniodev.composesimpleproject.view.component
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
@@ -10,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImagePainter
 import coil.compose.SubcomposeAsyncImage
@@ -38,14 +42,26 @@ fun FullDogImage(url: String?) {
         ) {
             val state = painter.state
             if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
-                CircularProgressIndicator(
-                    Modifier.padding(10.dp),
-                    Color.Green,
-                    2.dp
-                )
+                Row(
+                    Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(48.dp),
+                        Color.Green,
+                        2.dp
+                    )
+                }
             } else {
                 SubcomposeAsyncImageContent()
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FullDogImageTest() {
+    FullDogImage(url = "")
 }
